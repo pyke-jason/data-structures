8ac90e3ee4ee04f789ea8bb45e3d02e965aec026
Jason Pyke <pyke.jason1@gmail.com>
2020-06-12 11:10:48 -0700

    Implement seam carving using sentinel node

--- seamcarving/src/seamcarving/DijkstraSeamFinder.java
+++ seamcarving/src/seamcarving/DijkstraSeamFinder.java
@@ -3,14 +3,19 @@ package seamcarving;
 import graphs.Edge;
 import graphs.Graph;
 import graphs.shortestpaths.DijkstraShortestPathFinder;
+import graphs.shortestpaths.ShortestPath;
 import graphs.shortestpaths.ShortestPathFinder;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 public class DijkstraSeamFinder implements SeamFinder {
-    // TODO: replace all 4 references to "Object" on the line below with whatever vertex type
-    //  you choose for your graph
-    private final ShortestPathFinder<Graph<Object, Edge<Object>>, Object, Edge<Object>> pathFinder;
+    private final ShortestPathFinder<SeamGraph, RelativeVertex, Edge<RelativeVertex>> pathFinder;
 
     public DijkstraSeamFinder() {
         this.pathFinder = createPathFinder();
@@ -26,13 +31,121 @@ public class DijkstraSeamFinder implements SeamFinder {
 
     @Override
     public List<Integer> findHorizontalSeam(double[][] energies) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return findSeam(energies, false, energies.length);
     }
 
     @Override
     public List<Integer> findVerticalSeam(double[][] energies) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return findSeam(energies, true, energies[0].length);
+    }
+
+    private List<Integer> findSeam(double[][] energies, boolean vertical, int length) {
+        List<Integer> result = new LinkedList<>();
+        SeamGraph graph = new SeamGraph(energies, vertical);
+        RelativeVertex start = new RelativeVertex(vertical, 0, -1);
+        RelativeVertex end = new RelativeVertex(vertical, 0, length);
+        ShortestPath<RelativeVertex, Edge<RelativeVertex>> res = pathFinder.findShortestPath(graph, start, end);
+        if (!res.exists()) {
+            return new ArrayList<>();
+        }
+        for (RelativeVertex rv : res.vertices()) {
+            if (rv.isSentinel(length)) {
+                continue;
+            }
+            if (rv.forward > result.size() - 1) {
+                result.add(rv.lateral);
+            } else {
+                result.add(rv.forward, rv.lateral);
+            }
+        }
+        return result;
+    }
+
+    public class SeamGraph implements Graph<RelativeVertex, Edge<RelativeVertex>> {
+        private double[][] energies;
+        private int length;
+        private int width;
+        private boolean vertical;
+
+        public SeamGraph(double[][] energies, boolean vertical) {
+            this.energies = energies;
+            this.vertical = vertical;
+            length = vertical ? energies[0].length : energies.length;
+            width = vertical ? energies.length : energies[0].length;
+        }
+
+        @Override
+        public Collection<Edge<RelativeVertex>> outgoingEdgesFrom(RelativeVertex vert) {
+            Set<Edge<RelativeVertex>> res = new HashSet<>();
+            if (vert.forward < 0) {
+                for (int i = 0; i < width; i++) {
+                    double energy = vertical ? energies[i][0] : energies[0][i];
+                    res.add(new Edge<>(vert, new RelativeVertex(vertical, i, 0), energy));
+                }
+            } else if (vert.forward == length - 1) {
+                res.add(new Edge<>(vert, new RelativeVertex(vertical, 0, length), 0));
+            } else {
+                for (int i = -1; i <= 1; i++) {
+                    int x = vert.getX(i, 1);
+                    int y = vert.getY(i, 1);
+                    if (x > 0 && y > 0 && x < energies.length && y < energies[0].length) {
+                        RelativeVertex adjacentVert = new RelativeVertex(vertical, vert.lateral + i, vert.forward + 1);
+                        res.add(new Edge<>(vert, adjacentVert, energies[x][y]));
+                    }
+                }
+            }
+            return res;
+        }
+    }
+
+    public class RelativeVertex {
+        public int lateral;
+        public int forward;
+        public boolean vertical;
+
+        public boolean isSentinel(int length) {
+            return forward < 0 || forward == length;
+        }
+
+        public int getX() {
+            return getX(0, 0);
+        }
+
+        public int getY() {
+            return getY(0, 0);
+        }
+
+        public int getX(int lateralOffset, int forwardOffset) {
+            return vertical ? lateral + lateralOffset : forward + forwardOffset;
+        }
+
+        public int getY(int lateralOffset, int forwardOffset) {
+            return vertical ? forward + forwardOffset : lateral + lateralOffset;
+        }
+
+        public RelativeVertex(boolean vertical, int lateral, int forward) {
+            this.vertical = vertical;
+            this.lateral = lateral;
+            this.forward = forward;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            RelativeVertex that = (RelativeVertex) o;
+            return lateral == that.lateral &&
+                forward == that.forward &&
+                vertical == that.vertical;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(lateral, forward, vertical);
+        }
     }
 }
--- seamcarving/src/seamcarving/DualGradientEnergyFunction.java
+++ seamcarving/src/seamcarving/DualGradientEnergyFunction.java
@@ -2,10 +2,85 @@ package seamcarving;
 
 import edu.princeton.cs.algs4.Picture;
 
+import java.awt.Color;
+
 public class DualGradientEnergyFunction implements EnergyFunction {
+
+    private enum Channel {
+        RED,
+        GREEN,
+        BLUE
+    }
+
+
     @Override
     public double apply(Picture picture, int x, int y) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        int w = picture.width();
+        int h = picture.height();
+        double rx;
+        double ry;
+        double gx;
+        double gy;
+        double bx;
+        double by;
+        if (x == 0 || x == w - 1) {
+            int d = getDelta(x);
+            Color c1 = picture.get(x, y);
+            Color c2 = picture.get(x + d, y);
+            Color c3 = picture.get(x + 2 * d, y);
+            rx = getForwardBackwardGradient(Channel.RED, c1, c2, c3);
+            gx = getForwardBackwardGradient(Channel.GREEN, c1, c2, c3);
+            bx = getForwardBackwardGradient(Channel.BLUE, c1, c2, c3);
+        } else {
+            Color left = picture.get(x - 1, y);
+            Color right = picture.get(x - 1, y);
+            rx = getCentralGradient(Channel.RED, right, left);
+            gx = getCentralGradient(Channel.GREEN, right, left);
+            bx = getCentralGradient(Channel.BLUE, right, left);
+        }
+        if (y == 0 || y == h - 1) {
+            int d = getDelta(y);
+            Color c1 = picture.get(x, y);
+            Color c2 = picture.get(x, y + d);
+            Color c3 = picture.get(x, y + 2 * d);
+            ry = getForwardBackwardGradient(Channel.RED, c1, c2, c3);
+            gy = getForwardBackwardGradient(Channel.GREEN, c1, c2, c3);
+            by = getForwardBackwardGradient(Channel.BLUE, c1, c2, c3);
+        } else {
+            Color up = picture.get(x, y + 1);
+            Color down = picture.get(x, y - 1);
+            ry = getCentralGradient(Channel.RED, up, down);
+            gy = getCentralGradient(Channel.GREEN, up, down);
+            by = getCentralGradient(Channel.BLUE, up, down);
+        }
+        double gradxsqr = Math.pow(rx, 2) + Math.pow(gx, 2) + Math.pow(bx, 2);
+        double gradysqr = Math.pow(ry, 2) + Math.pow(gy, 2) + Math.pow(by, 2);
+        return Math.sqrt(gradxsqr + gradysqr);
     }
+
+    private int getDelta(int value) {
+        return value > 0 ? -1 : 1;
+    }
+
+    private double getForwardBackwardGradient(Channel c, Color c1, Color c2, Color c3) {
+        return -3 * getChannel(c1, c) + 4 * getChannel(c2, c) - getChannel(c3, c);
+    }
+
+    private double getCentralGradient(Channel c, Color c1, Color c2) {
+        return getChannel(c1, c) - getChannel(c2, c);
+    }
+
+    private int getChannel(Color col, Channel c) {
+        switch (c) {
+            case RED:
+                return col.getRed();
+            case GREEN:
+                return col.getGreen();
+            case BLUE:
+                return col.getBlue();
+            default:
+                throw new IllegalStateException("Unexpected value: " + c);
+        }
+    }
+
 }
